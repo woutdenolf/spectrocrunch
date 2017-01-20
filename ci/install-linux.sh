bd087329ea57694e783d7da6f78a9ca576f1db72
woutdenolf <woutdenolf@users.sf.net>
2017-01-21 00:51:15 +0100

    More ci stuff.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -3,6 +3,18 @@
 cwd=$(pwd)
 echo $cwd
 
+echo "Installing curl ..."
+$version = curl_version();
+$ssl_supported= ($version['features'] & CURL_VERSION_SSL);
+echo $version
+echo $version['features']
+echo $ssl_supported
+
+#./configure --with-ssl
+#make
+#sudo make install
+#cd $cwd
+
 echo "Installing cmake ..."
 wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
 tar xf cmake-3.2.2.tar.gz
