1f61c3b686fe46d6d48713abbb76e0693488a046
woutdenolf <woutdenolf@users.sf.net>
2017-01-21 20:45:30 +0100

    More ci stuff.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -11,11 +11,11 @@ wget --no-check-certificate -q http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.g
 tar xf cmake-3.7.2.tar.gz
 cd cmake-3.7.2
 echo -e "${hcol}Configure cmake ...${ncol}"
-./configure 1>/dev/null
+./configure
 echo -e "${hcol}Build cmake ...${ncol}"
-make 1>/dev/null
+make -s
 echo -e "${hcol}Install cmake ...${ncol}"
-sudo make install 1>/dev/null
+sudo make install -s
 
 cd $TRAVIS_BUILD_DIR
 
@@ -27,16 +27,15 @@ git clone -b master https://github.com/kaspermarstal/SimpleElastix
 mkdir build
 cd build
 echo -e "${hcol}Configure SimpleElastix ...${ncol}"
-#cmake ../SimpleElastix/SuperBuild 1>/dev/null
-cmake
+cmake ../SimpleElastix/SuperBuild 1>/dev/null
 echo -e "${hcol}Build SimpleElastix ...${ncol}"
 #travis_wait 30 mvn make 1>/dev/null
-make
+make -s
 echo -e "${hcol}Install SimpleElastix ...${ncol}"
 ls -R ./SimpleITK-build
-#cd ./SimpleITK-build/Wrapping/Python/Packaging
-cd ./SimpleITK-build/Wrapping/PythonPackage
-python setup.py install 1>/dev/null
+cd ./SimpleITK-build/Wrapping/Python/Packaging
+#cd ./SimpleITK-build/Wrapping/PythonPackage
+python setup.py install
 
 cd $TRAVIS_BUILD_DIR
 
