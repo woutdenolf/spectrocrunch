5ad7d666f0bed22824355454a47543e766d4ac69
woutdenolf <woutdenolf@users.sf.net>
2017-01-21 02:02:15 +0100

    More ci stuff.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -10,9 +10,9 @@ echo "Installing curl ..."
 #cd $cwd
 
 echo "Installing cmake ..."
-wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
-tar xf cmake-3.2.2.tar.gz
-cd cmake-3.2.2
+wget --no-check-certificate http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.gz
+tar xf cmake-3.7.2.tar.gz
+cd cmake-3.7.2
 ./configure
 make
 sudo make install
