d39a638b7641157e6738be02ba1d22f9af3f9cdf
woutdenolf <woutdenolf@users.sf.net>
2017-01-21 23:21:52 +0100

    More ci stuff.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
 # Minimizing output:
-#   1. 1>/dev/null  (problem is that no output will stop the job and travis_wait does not work well)
-#   2. make -s | grep -v '%]' | grep -v '^--'
+#   1. longtask ...
+#   2. make -s | grep -v '%]' | grep -v '^--' | ...
 #   3. configure | grep -v '^--'
+
+function longtask() {
+    # $@: all parameters as a string
+    # $!: PID of last job running in background.
+    local cmd="$@"
+    $cmd & 
+    while true; do
+        ps -p$! 1>/dev/null
+        if [ $? = 0 ]; then
+         echo "still running: $cmd"; sleep 1m
+        else
+         echo "$cmd finished"
+         break
+        fi
+    done
+}
 
 hcol='\033[0;35m'
 ncol='\033[0m'
+
+#============cmake============
 
 echo -e "${hcol}Download cmake ...${ncol}"
 cd $CACHED_FOLDER
@@ -15,14 +33,17 @@ cd cmake
 wget --no-check-certificate -q http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.gz
 tar -xzf cmake-3.7.2.tar.gz
 cd cmake-3.7.2
+
 echo -e "${hcol}Configure cmake ...${ncol}"
 ./configure | grep -v '^--'
+
 echo -e "${hcol}Build cmake ...${ncol}"
 make -s | grep -v '%]' | grep -v '^--'
-echo -e "${hcol}Install cmake ...${ncol}"
-sudo make install -s
 
-cd $TRAVIS_BUILD_DIR
+echo -e "${hcol}Install cmake ...${ncol}"
+sudo make install -s | grep -v '%]' | grep -v '^--'
+
+#============SimpleElastix============
 
 echo -e "${hcol}Download SimpleElastix ...${ncol}"
 cd $CACHED_FOLDER
@@ -31,16 +52,20 @@ cd simpleelastix
 git clone -b master https://github.com/kaspermarstal/SimpleElastix
 mkdir build
 cd build
+
 echo -e "${hcol}Configure SimpleElastix ...${ncol}"
-cmake -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_MESSAGE=NEVER ../SimpleElastix/SuperBuild 1>/dev/null
+longtask cmake -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_MESSAGE=NEVER ../SimpleElastix/SuperBuild
+
 echo -e "${hcol}Build SimpleElastix ...${ncol}"
-#travis_wait 30 mvn make 1>/dev/null
-make -s | grep -v '%]' | grep -v '^--'
+longtask make -s | grep -v '%]' | grep -v '^--' | grep -v '^Installing'
+
 echo -e "${hcol}Install SimpleElastix ...${ncol}"
 ls -R ./SimpleITK-build
 cd ./SimpleITK-build/Wrapping/Python/Packaging
 #cd ./SimpleITK-build/Wrapping/PythonPackage
 python setup.py install
+
+#============cleanup============
 
 cd $TRAVIS_BUILD_DIR
 
