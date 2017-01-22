008e8f88f3b6603ca475a5373fd89ccc0f76ac95
woutdenolf <woutdenolf@users.sf.net>
2017-01-22 16:10:52 +0100

    More ci stuff.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -2,7 +2,13 @@
 
 #============definitions============
 
-function longtask() {
+function reduceoutput() {
+    # Usage:
+    #   reduceoutput make
+    #
+    # Reducing output can also be done with ... | grep -v '%]' | grep -v '^--'
+
+    # Travis needs output every 10 minutes, otherwise it will shut down
     # $@: all parameters as a string
     # $!: PID of last job running in background.
     local cmd="$@"
@@ -28,30 +34,30 @@ function savestate() {
 
 function initstate() {
     if [[ -v $1 ]]; then
-        echo -e "${hcol}Status $1 is currently ${!1}${ncol}"
+        echo -e "${hcol}Status $1 is already ${!1}${ncol}"
     else
         export "$1"=0
-        echo -e "${hcol}Status $1 is initialized ${ncol}"
+        echo -e "${hcol}Status $1 is initialized to ${ncol}"
     fi 
 }
 
 function incstate() {
     let "$1++"
-    
+    echo -e "${hcol}Status $1 is incremented to ${!1}${ncol}"
 }
 
 function incandsavestate() {
-    let "$1++"
+    incstate $1
     savestate $1
 }
 
-function restorestate() {
+function restoresavedstate() {
     if [ -f $ENV_FILE ]; then
         source $ENV_FILE
     fi    
 }
 
-function clearstate() {
+function clearsavedstate() {
     if [ -f $ENV_FILE ]; then
         rm $ENV_FILE
     fi    
@@ -59,7 +65,7 @@ function clearstate() {
 
 #============init============
 
-restorestate
+restoresavedstate
 initstate STATE_CMAKE
 initstate STATE_SIMPLEELASTIX
 
@@ -70,14 +76,14 @@ STATE=1
 
 if [ "$STATE_CMAKE" -lt "$STATE" ]; then
     echo -e "${hcol}Download cmake ...${ncol}"
-    mkdir cmake
+    mkdir -p cmake
     cd cmake
     wget --no-check-certificate -q http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.gz
     tar -xzf cmake-3.7.2.tar.gz
-
     cd cmake-3.7.2
+
     echo -e "${hcol}Configure cmake ...${ncol}"
-    ./configure | grep -v '^--'
+    ./configure
 
     incandsavestate STATE_CMAKE
 else
@@ -88,7 +94,7 @@ incstate STATE
 
 if [ "$STATE_CMAKE" -lt "$STATE" ]; then
     echo -e "${hcol}Build cmake ...${ncol}"
-    make -s | grep -v '%]' | grep -v '^--'
+    make -s
 
     incandsavestate STATE_CMAKE
 fi
@@ -97,7 +103,7 @@ incstate STATE
 
 if [ "$STATE_CMAKE" -lt "$STATE" ]; then
     echo -e "${hcol}Install cmake ...${ncol}"
-    sudo make install -s | grep -v '%]' | grep -v '^--'
+    sudo make install -s
 
     incandsavestate STATE_CMAKE
 fi
@@ -109,11 +115,10 @@ STATE=1
 
 if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
     echo -e "${hcol}Download SimpleElastix ...${ncol}"
-    cd $CACHED_FOLDER
-    mkdir simpleelastix
+    mkdir -p simpleelastix
     cd simpleelastix
     git clone -b master https://github.com/kaspermarstal/SimpleElastix
-    mkdir build
+    mkdir -p build
     cd build
 
     incandsavestate STATE_SIMPLEELASTIX
@@ -126,7 +131,7 @@ incstate STATE
 OMP_NUM_THREADS=2
 if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
     echo -e "${hcol}Configure SimpleElastix ...${ncol}"
-    longtask cmake -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_MESSAGE=NEVER -DBUILD_TESTING=OFF ../SimpleElastix/SuperBuild
+    cmake -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_MESSAGE=NEVER -DBUILD_TESTING=OFF ../SimpleElastix/SuperBuild
 
     incandsavestate STATE_SIMPLEELASTIX
 fi
@@ -135,8 +140,7 @@ incstate STATE
 
 if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
     echo -e "${hcol}Build SimpleElastix ...${ncol}"
-    #longtask make -s | grep -v '%]' | grep -v '^--' | grep -v '^Installing'
-    make -s -j2 | grep -v '%]'
+    make -s -j2
 
     incandsavestate STATE_SIMPLEELASTIX
 fi
@@ -153,7 +157,7 @@ if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
 fi
 
 #============cleanup============
-clearstate
+clearsavedstate
 savestate STATE_CMAKE
 savestate STATE_SIMPLEELASTIX
 
