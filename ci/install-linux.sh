b6b013ac98add6a5ca5116b3a05d9c27b0b2b931
woutdenolf <woutdenolf@users.sf.net>
2017-01-22 15:54:21 +0100

    More ci stuff.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Minimizing output:
-#   1. longtask ...
-#   2. make -s | grep -v '%]' | grep -v '^--' | ...
-#   3. configure | grep -v '^--'
+#============definitions============
 
 function longtask() {
     # $@: all parameters as a string
@@ -21,53 +18,144 @@ function longtask() {
     done
 }
 
-hcol='\033[0;35m'
-ncol='\033[0m'
+export ENV_FILE=$CACHED_FOLDER/env.dat
+export hcol='\033[0;35m'
+export ncol='\033[0m'
+
+function savestate() {
+    echo "export" "$1"="${!1}" >> $ENV_FILE
+}
+
+function initstate() {
+    if [[ -v $1 ]]; then
+        echo -e "${hcol}Status $1 is currently ${!1}${ncol}"
+    else
+        export "$1"=0
+        echo -e "${hcol}Status $1 is initialized ${ncol}"
+    fi 
+}
+
+function incstate() {
+    let "$1++"
+    
+}
+
+function incandsavestate() {
+    let "$1++"
+    savestate $1
+}
+
+function restorestate() {
+    if [ -f $ENV_FILE ]; then
+        source $ENV_FILE
+    fi    
+}
+
+function clearstate() {
+    if [ -f $ENV_FILE ]; then
+        rm $ENV_FILE
+    fi    
+}
+
+#============init============
+
+restorestate
+initstate STATE_CMAKE
+initstate STATE_SIMPLEELASTIX
 
 #============cmake============
 
-echo -e "${hcol}Download cmake ...${ncol}"
 cd $CACHED_FOLDER
-mkdir cmake
-cd cmake
-wget --no-check-certificate -q http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.gz
-tar -xzf cmake-3.7.2.tar.gz
-cd cmake-3.7.2
+STATE=1
 
-echo -e "${hcol}Configure cmake ...${ncol}"
-./configure | grep -v '^--'
+if [ "$STATE_CMAKE" -lt "$STATE" ]; then
+    echo -e "${hcol}Download cmake ...${ncol}"
+    mkdir cmake
+    cd cmake
+    wget --no-check-certificate -q http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.gz
+    tar -xzf cmake-3.7.2.tar.gz
 
-echo -e "${hcol}Build cmake ...${ncol}"
-make -s | grep -v '%]' | grep -v '^--'
+    cd cmake-3.7.2
+    echo -e "${hcol}Configure cmake ...${ncol}"
+    ./configure | grep -v '^--'
 
-echo -e "${hcol}Install cmake ...${ncol}"
-sudo make install -s | grep -v '%]' | grep -v '^--'
+    incandsavestate STATE_CMAKE
+else
+    cd cmake/cmake-3.7.2
+fi
+
+incstate STATE
+
+if [ "$STATE_CMAKE" -lt "$STATE" ]; then
+    echo -e "${hcol}Build cmake ...${ncol}"
+    make -s | grep -v '%]' | grep -v '^--'
+
+    incandsavestate STATE_CMAKE
+fi
+
+incstate STATE
+
+if [ "$STATE_CMAKE" -lt "$STATE" ]; then
+    echo -e "${hcol}Install cmake ...${ncol}"
+    sudo make install -s | grep -v '%]' | grep -v '^--'
+
+    incandsavestate STATE_CMAKE
+fi
 
 #============SimpleElastix============
 
-echo -e "${hcol}Download SimpleElastix ...${ncol}"
 cd $CACHED_FOLDER
-mkdir simpleelastix
-cd simpleelastix
-git clone -b master https://github.com/kaspermarstal/SimpleElastix
-mkdir build
-cd build
+STATE=1
 
-echo -e "${hcol}Configure SimpleElastix ...${ncol}"
+if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
+    echo -e "${hcol}Download SimpleElastix ...${ncol}"
+    cd $CACHED_FOLDER
+    mkdir simpleelastix
+    cd simpleelastix
+    git clone -b master https://github.com/kaspermarstal/SimpleElastix
+    mkdir build
+    cd build
+
+    incandsavestate STATE_SIMPLEELASTIX
+else
+    cd simpleelastix/build
+fi
+
+incstate STATE
+
 OMP_NUM_THREADS=2
-longtask cmake -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_MESSAGE=NEVER -DBUILD_TESTING=OFF ../SimpleElastix/SuperBuild
+if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
+    echo -e "${hcol}Configure SimpleElastix ...${ncol}"
+    longtask cmake -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_MESSAGE=NEVER -DBUILD_TESTING=OFF ../SimpleElastix/SuperBuild
 
-echo -e "${hcol}Build SimpleElastix ...${ncol}"
-#longtask make -s | grep -v '%]' | grep -v '^--' | grep -v '^Installing'
-make -s -j2 | grep -v '%]'
+    incandsavestate STATE_SIMPLEELASTIX
+fi
 
-echo -e "${hcol}Install SimpleElastix ...${ncol}"
-ls -R ./SimpleITK-build
-cd ./SimpleITK-build/Wrapping/Python/Packaging
-#cd ./SimpleITK-build/Wrapping/PythonPackage
-python setup.py install
+incstate STATE
+
+if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
+    echo -e "${hcol}Build SimpleElastix ...${ncol}"
+    #longtask make -s | grep -v '%]' | grep -v '^--' | grep -v '^Installing'
+    make -s -j2 | grep -v '%]'
+
+    incandsavestate STATE_SIMPLEELASTIX
+fi
+
+incstate STATE
+
+if [ "$STATE_SIMPLEELASTIX" -lt "$STATE" ]; then
+    echo -e "${hcol}Install SimpleElastix ...${ncol}"
+    cd ./SimpleITK-build/Wrapping/Python/Packaging
+    #cd ./SimpleITK-build/Wrapping/PythonPackage
+    python setup.py install
+
+    incandsavestate STATE_SIMPLEELASTIX
+fi
 
 #============cleanup============
+clearstate
+savestate STATE_CMAKE
+savestate STATE_SIMPLEELASTIX
 
 cd $TRAVIS_BUILD_DIR
 
