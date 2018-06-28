0ed302e116246bf9cdd3734aa8dae5d45f9a7d1f
woutdenolf <woutdenolf@users.sf.net>
2018-06-28 16:13:39 +0200

    Build tools: fix prebuild download

--- ci/travis-linux-prepare.sh
+++ ci/travis-linux-prepare.sh
@@ -3,13 +3,17 @@
 # This script will install pre-builds on Travis.
 # 
 
+# Add package repositories
+sudo -E add-apt-repository universe
+sudo -E apt-get update
+
 # Download pre-build libraries
 PYTHONV=`python -c "import sys;t='{v[0]}.{v[1]}.{v[2]}'.format(v=list(sys.version_info[:3]));print(t)"`
 
 cd ${BUILD_FOLDER}
 DEP_FOLDER=${BUILD_FOLDER}/dep_{PYTHONV}
 
-if [ ! -d ${DEP_FOLDER} ]; then
+if [[ ! -d ${DEP_FOLDER} ]]; then
     FILE=spectrocrunch.travis.python${PYTHONV}.tgz
     LINK1=http://ftp.esrf.fr/tmp/${FILE}
     LINK2=https://transfer.sh/12avMO/${FILE}
@@ -17,16 +21,16 @@ if [ ! -d ${DEP_FOLDER} ]; then
     # Download to cache folder
     cd ${CACHED_FOLDER}
     ls -all
-    if [ ! -d ${MARKER} ]; then
+    if [[ ! -d ${DEP_FOLDER} ]]; then
         echo "Download pre-build libraries ..."
         wget ${LINK1}
         ls -all
-        if [ ! -f ${FILE} ]; then
+        if [[ ! -f ${FILE} ]]; then
             wget ${LINK2}
         fi
 
         # Unpack in build folder
-        if [ -f ${FILE} ]; then
+        if [[ -f ${FILE} ]]; then
             echo "Unpack pre-build libraries ..."
             tar -xzf ${FILE} -C ${BUILD_FOLDER}
             rm -f ${FILE}
@@ -37,7 +41,7 @@ if [ ! -d ${DEP_FOLDER} ]; then
 fi
 
 # List pre-build libraries   
-if [ -d ${DEP_FOLDER} ]; then
+if [[ -d ${DEP_FOLDER} ]]; then
     echo "Pre-build libraries:"
     ls ${DEP_FOLDER}
 else
@@ -48,8 +52,6 @@ fi
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
-# Add package repositories
-sudo -E add-apt-repository universe
-sudo -E apt-get update
+
 
 
