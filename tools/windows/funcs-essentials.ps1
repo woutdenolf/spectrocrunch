fdcf2ea73b91ceefd7e2c2469354cef5f97cdfc6
woutdenolf <woutdenolf@users.sf.net>
2018-12-23 22:25:54 +0100

    [py3] + windows distribution

---
+++ tools/windows/funcs-essentials.ps1
@@ -0,0 +1,23 @@
+#
+# Essentials for installation and downloading
+#
+
+
+function require_build_essentials()
+{
+}
+
+
+function require_web_essentials()
+{
+    require_web_access
+}
+
+
+function require_web_access()
+{
+    if ((Get-WmiObject Win32_ComputerSystem).Domain -eq "esrf.fr") {
+        netsh winhttp set proxy "http://proxy.esrf.fr:3128"
+        (New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+    }
+}
