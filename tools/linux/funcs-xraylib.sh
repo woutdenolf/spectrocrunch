86f41253e86f4b7fb5f9e6a7d21f00ab01cfb883
woutdenolf <woutdenolf@users.sf.net>
2019-02-16 19:17:39 +0100

    [ci]

--- tools/linux/funcs-xraylib.sh
+++ tools/linux/funcs-xraylib.sh
@@ -43,10 +43,10 @@ function xraylib_download()
 
 function xraylib_install_fromsource()
 {
-    #if [[ ! -d xraylib && ${ARG_SKIPLONG} == true ]]; then
-    #    cprint "Skipping xraylib installation"
-    #    return
-    #fi
+    if [[ ! -d xraylib && ${ARG_SKIPLONG} == true ]]; then
+        cprint "Skipping xraylib installation"
+        return
+    fi
 
     local restorewd=$(pwd)
 
